748a9d573da2f100fe84f8646ce8aae278eb20e2
AIDStudio <aidstudio21@gmail.com>
2026-05-22 10:37:01 +0200

    Refactor SceneSimplifier to use new graphics mode methods

--- src/main/java/com/optimobileplusz/module/SceneSimplifier.java
+++ src/main/java/com/optimobileplusz/module/SceneSimplifier.java
@@ -3,17 +3,8 @@ package com.optimobileplusz.module;
 import com.optimobileplusz.config.OptiMobileConfig;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.GraphicsMode;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 
 public class SceneSimplifier {
-    
-    // Gyorsítótár (cache), hogy a Reflection csak egyszer fusson le, és megmaradjon a max sebesség
-    private static boolean resolved = false;
-    private static Field directEnumField = null;
-    private static Field simpleOptionField = null;
-    private static Method simpleOptionGetter = null;
-    private static Method setValueMethod = null;
 
     public static void update() {
         if (!OptiMobileConfig.sceneSimplifierEnabled) {
@@ -25,61 +16,14 @@ public class SceneSimplifier {
             return;
         }
 
-        GraphicsMode targetMode = OptiMobileConfig.frameBudgetEnabled ? GraphicsMode.FAST : GraphicsMode.FANCY;
-
-        // Ha még nem kerestük meg a pontos változót, most megtesszük
-        if (!resolved) {
-            resolveMapping(client.options);
-            resolved = true;
-        }
-
-        // Érték beállítása a megtalált és cache-elt útvonalon
-        try {
-            if (directEnumField != null) {
-                directEnumField.set(client.options, targetMode);
-            } else if (simpleOptionField != null) {
-                Object optionObj = simpleOptionField.get(client.options);
-                if (setValueMethod != null) setValueMethod.invoke(optionObj, targetMode);
-            } else if (simpleOptionGetter != null) {
-                Object optionObj = simpleOptionGetter.invoke(client.options);
-                if (setValueMethod != null) setValueMethod.invoke(optionObj, targetMode);
-            }
-        } catch (Exception e) {
-            // Csendes kivételkezelés, hogy ne spamkelje tele a logokat játék közben
-        }
-    }
-
-    private static void resolveMapping(Object options) {
-        try {
-            // 1. Esély: Régebbi verziók, ahol a mező közvetlenül egy GraphicsMode enum
-            for (Field f : options.getClass().getFields()) {
-                if (f.getType() == GraphicsMode.class) {
-                    directEnumField = f;
-                    return;
-                }
-            }
-
-            // 2. Esély: Újabb verziók, ahol a beállítás egy SimpleOption burkolóban van mezőként
-            for (Field f : options.getClass().getFields()) {
-                if (f.getType().getSimpleName().equals("SimpleOption") && f.getName().toLowerCase().contains("graphics")) {
-                    simpleOptionField = f;
-                    Object optionObj = f.get(options);
-                    setValueMethod = optionObj.getClass().getMethod("setValue", Object.class);
-                    return;
-                }
-            }
-
-            // 3. Esély: Legújabb verziók, ahol a SimpleOption objektumot egy getter metódus adja vissza
-            for (Method m : options.getClass().getMethods()) {
-                if (m.getReturnType().getSimpleName().equals("SimpleOption") && m.getName().toLowerCase().contains("graphics")) {
-                    simpleOptionGetter = m;
-                    Object optionObj = m.invoke(options);
-                    setValueMethod = optionObj.getClass().getMethod("setValue", Object.class);
-                    return;
-                }
-            }
-        } catch (Exception e) {
-            System.out.println("[OptiMobilePlusz] Nem sikerult automatikusan feloldani a grafikai modot.");
+        // 1.21-ben a GameOptions-ben a grafika már nem mező, hanem egy objektum, 
+        // ami a .getValue() és .setValue() metódusokat használja.
+        // A mező neve a Yarn mappingben 1.21-nél: graphicsMode
+        
+        if (OptiMobileConfig.frameBudgetEnabled) {
+            client.options.getGraphicsMode().setValue(GraphicsMode.FAST);
+        } else {
+            client.options.getGraphicsMode().setValue(GraphicsMode.FANCY);
         }
     }
 }
